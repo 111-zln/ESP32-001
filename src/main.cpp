0149c14ccd535499b24a70fa89b341dfa00ceb92
111-zln <2642986901@qq.com>
2026-08-10 15:49:16 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,20 @@ void setup()
 {
     Serial.begin(115200);
 
+    // 1. 硬件初始化
     board_.init();
 
+    // 2. 读取保存的配置
     loadConfig();
 
+    // 3. Service 初始化
     sensorService.init(board_.bme_, board_.co2_);
     wifiService.init();
 
+    // 4. App 初始化
     app_.init();
 
+    // 5. 创建 FreeRTOS Tasks
     createTasks();
 }
 
